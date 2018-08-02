14316f67cb245498202a0f3a4ec3875c696d8350
Maciej Baranowski <maciekdbaranowski@gmail.com>
2018-08-02 15:40:18 +0200

    add relative integral score

--- src/scores.py
+++ src/scores.py
@@ -3,3 +3,11 @@ def calculate_integral_score(results):
     for i in range(len(results) - 1):
         score += (results[i] + results[i + 1]) / 2.0
     return score
+
+
+def calculate_relative_integral_score(results):
+    score = 0
+    integral_base = results[0]
+    for i in range(len(results) - 1):
+        score += (results[i] + results[i + 1] - 2 * integral_base) / 2.0
+    return score
