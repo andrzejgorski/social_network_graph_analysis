16da90924021b54d3d3f127e5b7c2c32cca8efef
Andrzej Górski <andrzejgorski@supermond.com>
2018-08-04 18:21:34 +0200

    Remove ununsed functions.

--- src/charts.py
+++ src/charts.py
@@ -2,24 +2,6 @@ from matplotlib.ticker import MaxNLocator
 import matplotlib.pyplot as plt
 
 
-def get_ranking_result(graph_sets, boss, metric_cls):
-    return [
-        [
-            metric_cls(graph, boss).get_node_ranking(boss) for graph in g_set
-        ]
-        for g_set in graph_sets
-    ]
-
-
-def get_metric_values(graph_sets, boss, metric_cls):
-    return [
-        [
-            metric_cls(graph, boss).apply_metric(boss) for graph in g_set
-        ]
-        for g_set in graph_sets
-    ]
-
-
 def save_metric_ranking_plot(results, metric_name, label, output_file=None):
     output_format = '.jpeg'
 
